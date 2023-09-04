ab428660f187317614ab3be389d25a91cf7e819e
MauricioAndre <mauricioandrefernando@gmail.com>
2023-09-04 13:09:19 -0300

    commit  para prdoucao

--- integracao.py
+++ integracao.py
@@ -6,10 +6,10 @@ import dotenv
 dotenv.load_dotenv()
 token=os.getenv("TOKEN")
 
-# tipoxml='entregas'
-# uri=f"/CenterWeb/api/{token}/batch/schedules.xml"
-# query=queries.entregas
-# processa.processa(query,uri,tipoxml)
+tipoxml='entregas'
+uri=f"/CenterWeb/api/{token}/batch/schedules.xml"
+query=queries.entregas
+processa.processa(query,uri,tipoxml)
 
 
 tipoxml='items'
